ea7d404fd48244d4e5291b30326343c1bfd0fbf1
craftersmine <rodion.morozov.01@gmail.com>
2023-06-24 02:01:20 +0700

    Added project to a solution file

---
+++ craftersmine.League.CommunityDragon/CommunityDragon.cs
@@ -0,0 +1,12 @@
+﻿namespace craftersmine.League.CommunityDragon
+{
+    public class CommunityDragon
+    {
+        private HttpClient _httpClient;
+
+        public CommunityDragon(Version gameVersion)
+        {
+
+        }
+    }
+}
