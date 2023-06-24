10696f9a0f4c3406d43919d4fbd53e3cfa75ba1e
craftersmine <rodion.morozov.01@gmail.com>
2023-06-24 22:29:43 +0700

    Added all available League locales as static class

---
+++ craftersmine.League.CommunityDragon/LeagueLocales.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.League.CommunityDragon
+{
+    public static class LeagueLocales
+    {
+        public static readonly CultureInfo English = CultureInfo.InvariantCulture;
+        public static readonly CultureInfo Russian = new CultureInfo("ru-RU");
+        public static readonly CultureInfo Czech = new CultureInfo("cs-CZ");
+        public static readonly CultureInfo German = new CultureInfo("de-DE");
+        public static readonly CultureInfo Greek = new CultureInfo("el-GR");
+        public static readonly CultureInfo EnglishAU = new CultureInfo("en-AU");
+        public static readonly CultureInfo EnglishGB = new CultureInfo("en-GB");
+        public static readonly CultureInfo EnglishPH = new CultureInfo("en-PH");
+        public static readonly CultureInfo EnglishSG = new CultureInfo("en-SG");
+        public static readonly CultureInfo SpahishAR = new CultureInfo("es-AR");
+        public static readonly CultureInfo Spanish = new CultureInfo("es-ES");
+        public static readonly CultureInfo SpanishMX = new CultureInfo("es-MX");
+        public static readonly CultureInfo French = new CultureInfo("fr-FR");
+        public static readonly CultureInfo Hungarian = new CultureInfo("hu-HU");
+        public static readonly CultureInfo Italian = new CultureInfo("it-IT");
+        public static readonly CultureInfo Japanese = new CultureInfo("jp-JP");
+        public static readonly CultureInfo Korean = new CultureInfo("ko-KR");
+        public static readonly CultureInfo Polish = new CultureInfo("pl-PL");
+        public static readonly CultureInfo Portuguese = new CultureInfo("pt-BR");
+        public static readonly CultureInfo Romanian = new CultureInfo("ro-RO");
+        public static readonly CultureInfo Thai = new CultureInfo("th-TH");
+        public static readonly CultureInfo Turkish = new CultureInfo("th-TH");
+        public static readonly CultureInfo Vietnamese = new CultureInfo("vi-VN");
+        public static readonly CultureInfo Chinese = new CultureInfo("zh-CH");
+        public static readonly CultureInfo ChineseTW = new CultureInfo("zh-TW");
+        public static readonly CultureInfo ChineseMY = new CultureInfo("zh-MY");
+    }
+}
