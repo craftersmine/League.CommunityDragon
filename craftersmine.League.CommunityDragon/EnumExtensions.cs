8b6b1ebe3a53a0fe847bdca165a2848b9f00bc02
craftersmine <rodion.morozov.01@gmail.com>
2023-06-24 22:30:17 +0700

    Added enum extensions

---
+++ craftersmine.League.CommunityDragon/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.League.CommunityDragon
+{
+    public static class EnumExtensions
+    {
+        public static string GetJsonEnumValue(this Enum value)
+        {
+            if (value.GetType().GetMember(value.ToString())[0].GetCustomAttributes(typeof(JsonEnumValueAttribute), false)[0] is JsonEnumValueAttribute attribute)
+                return attribute.Value;
+            return value.ToString();
+        }
+    }
+}
