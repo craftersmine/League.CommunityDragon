a0bfa01957ec02a60256d5b603d10f832a05e274
craftersmine <rodion.morozov.01@gmail.com>
2023-06-24 22:30:01 +0700

    Added JSON enum value attribute

---
+++ craftersmine.League.CommunityDragon/JsonEnumValueAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.League.CommunityDragon
+{
+    internal class JsonEnumValueAttribute : Attribute
+    {
+        public string Value;
+
+        public JsonEnumValueAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+}
