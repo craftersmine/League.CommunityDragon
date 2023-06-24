91f18928d1ce174ad6d87ac429e8a8d755b211b6
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 03:17:19 +0700

    Added method to parse string into enum value

--- craftersmine.League.CommunityDragon/EnumExtensions.cs
+++ craftersmine.League.CommunityDragon/EnumExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,21 @@ namespace craftersmine.League.CommunityDragon
             if (value.GetType().GetMember(value.ToString())[0].GetCustomAttributes(typeof(JsonEnumValueAttribute), false)[0] is JsonEnumValueAttribute attribute)
                 return attribute.Value;
             return value.ToString();
+        }
+
+        public static T ParseJsonEnumValue<T>(this string val) where T : Enum
+        {
+            foreach (var field in typeof(T).GetFields())
+            {
+                if (field.GetCustomAttribute(typeof(JsonEnumValueAttribute)) is JsonEnumValueAttribute attribute)
+                {
+                    if (attribute.Value == val)
+                        return (T)field.GetValue(null);
+                    else if (field.Name == val)
+                        return (T)field.GetValue(null);
+                }
+            }
+            throw new ArgumentException("Unable to find value \"" + val + "\" for " + typeof(T).FullName + " enum", nameof(val));
         }
     }
 }
