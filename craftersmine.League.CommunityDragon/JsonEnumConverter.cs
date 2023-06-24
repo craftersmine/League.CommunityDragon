5c43bdc07871a3da1c75787b3e2d7a2e609960d4
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 03:16:38 +0700

    Added JSON enum converter

---
+++ craftersmine.League.CommunityDragon/JsonEnumConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace craftersmine.League.CommunityDragon
+{
+    internal class JsonEnumConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value is Enum && value.GetType().GetCustomAttributes(typeof(JsonEnumAttribute), false).Any())
+                writer.WriteValue(((Enum)value).GetJsonEnumValue());
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
