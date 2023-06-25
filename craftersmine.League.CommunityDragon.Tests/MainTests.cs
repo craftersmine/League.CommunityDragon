500c973d808f1b06c63752fa17343d595c0b7990
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 13:40:54 +0700

    Updated main tests

--- craftersmine.League.CommunityDragon.Tests/MainTests.cs
+++ craftersmine.League.CommunityDragon.Tests/MainTests.cs
@@ -16,15 +16,15 @@ namespace craftersmine.League.CommunityDragon.Tests
             CommunityDragon dragonEn = new CommunityDragon(new Version(13, 12), LeagueLocales.English);
             Assert.IsNotNull(dragonEn);
             Assert.IsTrue(dragonEn.MetadataUri.ToString() == string.Format(BaseUriFormat, "13.12", "default"));
-            Assert.IsTrue(dragonEn.AssetsUri.ToString() == string.Format(AssetsUriFormat, "13.12"));
+            Assert.IsTrue(dragonEn.ClientAssetsUri.ToString() == string.Format(AssetsUriFormat, "13.12"));
             Debug.WriteLine("Dragon Metadata EN-13.12: " + dragonEn.MetadataUri);
-            Debug.WriteLine("Dragon Assets EN-13.12: " + dragonEn.AssetsUri);
+            Debug.WriteLine("Dragon Assets EN-13.12: " + dragonEn.ClientAssetsUri);
             CommunityDragon dragonRu = new CommunityDragon(new Version(13, 12), LeagueLocales.Russian);
             Assert.IsNotNull(dragonRu);
             Assert.IsTrue(dragonRu.MetadataUri.ToString() == string.Format(BaseUriFormat, "13.12", "ru_ru"));
-            Assert.IsTrue(dragonRu.AssetsUri.ToString() == string.Format(AssetsUriFormat, "13.12"));
+            Assert.IsTrue(dragonRu.ClientAssetsUri.ToString() == string.Format(AssetsUriFormat, "13.12"));
             Debug.WriteLine("Dragon Metadata RU-13.12: " + dragonRu.MetadataUri);
-            Debug.WriteLine("Dragon Assets RU-13.12: " + dragonEn.AssetsUri);
+            Debug.WriteLine("Dragon Assets RU-13.12: " + dragonEn.ClientAssetsUri);
 
         }
     }
