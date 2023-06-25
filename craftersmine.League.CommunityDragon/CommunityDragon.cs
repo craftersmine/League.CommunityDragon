079727365a079035b07a455cc38115c5be1b6565
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 13:41:11 +0700

    Implemented basic constructor

--- craftersmine.League.CommunityDragon/CommunityDragon.cs
+++ craftersmine.League.CommunityDragon/CommunityDragon.cs
@@ -1,12 +1,29 @@
-﻿namespace craftersmine.League.CommunityDragon
+﻿using System.Globalization;
+
+namespace craftersmine.League.CommunityDragon
 {
     public class CommunityDragon
     {
+        private const string BaseUriFormat = "https://raw.communitydragon.org/{0}/plugins/rcp-be-lol-game-data/global/{1}/v1/";
+        private const string ClientAssetsUriFormat =
+            "https://raw.communitydragon.org/{0}/plugins/rcp-be-lol-game-data/global/default/assets/"; 
+
         private HttpClient _httpClient;
 
-        public CommunityDragon(Version gameVersion)
-        {
+        public Uri MetadataUri { get; }
+        public Uri ClientAssetsUri { get; }
 
+        public CommunityDragon(Version gameVersion, CultureInfo locale)
+        {
+            _httpClient = new HttpClient();
+
+            string localeStr;
+            if (Equals(locale, CultureInfo.InvariantCulture))
+                localeStr = "default";
+            else localeStr = locale.Name.ToLower().Replace("-", "_");
+            string gameVer = gameVersion.Major + "." + gameVersion.Minor;
+            MetadataUri = new Uri(string.Format(BaseUriFormat, gameVer, localeStr));
+            ClientAssetsUri = new Uri(string.Format(ClientAssetsUriFormat, gameVer));
         }
     }
 }
