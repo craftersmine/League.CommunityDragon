6d80e6f46781021bfede6cb9ea0bcb498ca0d4d1
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 18:53:25 +0700

    Added SummonerIconSet

---
+++ craftersmine.League.CommunityDragon/SummonerIconSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace craftersmine.League.CommunityDragon
+{
+    public class SummonerIconSet
+    {
+        [JsonProperty("id")]
+        public int Id { get; private set; }
+        [JsonProperty("hidden")]
+        public bool IsHidden { get; private set; }
+        [JsonProperty("displayName")]
+        public string DisplayName { get; private set; }
+        [JsonProperty("description")]
+        public string Description { get; private set; }
+        [JsonProperty("icons")]
+        public int[] IconIds { get; private set; }
+    }
+}
---
+++ craftersmine.League.CommunityDragon/SummonerIconSetCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.League.CommunityDragon
+{
+    public class SummonerIconSetCollection : IReadOnlyCollection<SummonerIconSet>
+    {
+        private readonly List<SummonerIconSet> _summonerIconSets = new List<SummonerIconSet>();
+
+        public SummonerIconSet this[int id]
+        {
+            get
+            {
+                SummonerIconSet? iconSet = _summonerIconSets.FirstOrDefault(i => i.Id == id);
+                if (iconSet is not null) return iconSet;
+
+                throw new KeyNotFoundException("Unable to find summoner icon with ID: " + id);
+            }
+        }
+
+        public SummonerIconSet this[string title]
+        {
+            get
+            {
+                SummonerIconSet? icon = _summonerIconSets.FirstOrDefault(i => i.DisplayName.Trim() == title);
+                if (icon is not null) return icon;
+
+                throw new KeyNotFoundException("Unable to find summoner icon with Title: " + title);
+            }
+        }
+
+        internal SummonerIconSetCollection(SummonerIconSet[]? iconSets)
+        {
+            if (iconSets is not null)
+                _summonerIconSets.AddRange(iconSets);
+        }
+
+        public IEnumerator<SummonerIconSet> GetEnumerator()
+        {
+            return _summonerIconSets.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => _summonerIconSets.Count;
+    }
+}
