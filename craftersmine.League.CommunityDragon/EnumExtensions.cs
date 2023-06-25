fa7f9a26fd47fa551d518c47ca778e2aebfaf73e
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 13:41:45 +0700

    Added non-generic parse method

--- craftersmine.League.CommunityDragon/EnumExtensions.cs
+++ craftersmine.League.CommunityDragon/EnumExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -14,6 +14,21 @@ namespace craftersmine.League.CommunityDragon
             if (value.GetType().GetMember(value.ToString())[0].GetCustomAttributes(typeof(JsonEnumValueAttribute), false)[0] is JsonEnumValueAttribute attribute)
                 return attribute.Value;
             return value.ToString();
+        }
+
+        public static object ParseJsonEnumValue(this string val, Type type)
+        {
+            foreach (var field in type.GetFields())
+            {
+                if (field.GetCustomAttribute(typeof(JsonEnumValueAttribute)) is JsonEnumValueAttribute attribute)
+                {
+                    if (attribute.Value == val)
+                        return field.GetValue(null)!;
+                    else if (field.Name == val)
+                        return field.GetValue(null)!;
+                }
+            }
+            throw new ArgumentException("Unable to find value \"" + val + "\" for " + type.FullName + " enum", nameof(val));
         }
 
         public static T ParseJsonEnumValue<T>(this string val) where T : Enum
