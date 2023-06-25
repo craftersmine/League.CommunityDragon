bedcace27cfa99615dfc144e761fd848bdf816fd
craftersmine <rodion.morozov.01@gmail.com>
2023-06-25 17:33:37 +0700

    Added request exception

---
+++ craftersmine.League.CommunityDragon/CommunityDragonRequestException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.League.CommunityDragon
+{
+    public class CommunityDragonRequestException : Exception
+    {
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public CommunityDragonRequestException(HttpStatusCode code)
+        {
+            StatusCode = code;
+        }
+
+        public CommunityDragonRequestException(HttpStatusCode code, string message) : base(message)
+        {
+            StatusCode = code;
+        }
+
+        public CommunityDragonRequestException(HttpStatusCode code, string message, Exception inner) : base(message, inner)
+        {
+            StatusCode = code;
+        }
+    }
+}
