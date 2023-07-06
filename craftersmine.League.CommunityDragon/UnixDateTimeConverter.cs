2625ad7711587d9e13d63456b3765e9f4f1f0ed8
craftersmine <rodion.morozov.01@gmail.com>
2023-07-06 21:44:21 +0700

    Added UNIX time converters

---
+++ craftersmine.League.CommunityDragon/UnixDateTimeConverter.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+
+namespace craftersmine.League.CommunityDragon
+{
+    internal class UnixDateTimeConverter : JsonConverter
+    {
+        private readonly bool useSeconds;
+
+        public UnixDateTimeConverter() : this(false) { }
+
+        public UnixDateTimeConverter(bool useSeconds)
+        {
+            this.useSeconds = useSeconds;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            long val;
+            if (value is DateTime)
+            {
+                if (!useSeconds)
+                    val = ((DateTime) value).ToUnixTimeMilliseconds();
+                else
+                    val = ((DateTime) value).ToUnixTimeSeconds();
+            }
+            else
+            {
+                throw new Exception("Expected DateTime value, got " + value.GetType().Name);
+            }
+            writer.WriteValue(val);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+                return new DateTime(1970, 1, 1);
+
+            long val = 0;
+
+            if (reader.TokenType != JsonToken.Integer)
+            {
+                if (reader.TokenType == JsonToken.String)
+                {
+                    if (reader.Value != null) val = long.Parse(reader.Value.ToString());
+                }
+                else
+                    throw new Exception("Expected integer value, got " + reader.TokenType);
+            }
+
+            long ticks;
+            if (val == 0)
+                ticks = (long) (reader.Value ?? 0);
+            else ticks = val;
+
+            if (!useSeconds)
+                return ticks.FromUnixTimeMilliseconds();
+            else return ticks.FromUnixTimeSeconds();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(int);
+        }
+    }
+}
---
+++ craftersmine.League.CommunityDragon/UnixTimeSpanConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace craftersmine.League.CommunityDragon
+{
+    internal class UnixTimeSpanConverter : JsonConverter
+    {
+        private bool useSeconds = false;
+
+        public UnixTimeSpanConverter() : this(false) {}
+
+        public UnixTimeSpanConverter(bool useSeconds)
+        {
+            this.useSeconds = useSeconds;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is TimeSpan ts)
+            {
+                writer.WriteValue((long)ts.TotalMilliseconds);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+                return new TimeSpan(0, 0, 0, 0, 0);
+
+            if (reader.Value != null)
+            {
+                if (reader.TokenType != JsonToken.Integer && reader.TokenType != JsonToken.Float)
+                    throw new Exception("Expected integer or float value, got " + reader.TokenType);
+                if (reader.TokenType == JsonToken.Integer)
+                {
+                    long msTs = (long) reader.Value;
+                    if (useSeconds)
+                        return TimeSpan.FromSeconds(msTs);
+                    else
+                        return TimeSpan.FromMilliseconds(msTs);
+                }
+                else
+                {
+                    if (useSeconds)
+                        return TimeSpan.FromSeconds((double) reader.Value);
+                    else
+                        return TimeSpan.FromMilliseconds((double) reader.Value);
+                }
+            }
+
+            return new TimeSpan(0, 0, 0, 0, 0);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TimeSpan);
+        }
+    }
+}
